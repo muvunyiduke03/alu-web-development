3abbfcec5eaf081ff75d0daba14ede394844b7e0
muvunyiduke03 <d.ndamagem@alustudent.com>
2026-09-20 07:15:53 +0000

    New commit on task 2

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - raises a 401 error to trigger the Unauthorized error handler
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error to trigger the Forbidden error handler
+    """
+    abort(403)
